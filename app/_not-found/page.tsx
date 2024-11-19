57d1cbdf6cf41d309c1a6573351172d34eb753f0
Dakutsuki <demomega9@gmail.com>
2024-11-19 06:36:04 +0100

    move

---
+++ app/_not-found/page.tsx
@@ -0,0 +1,13 @@
+// app/_not-found/page.tsx
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
